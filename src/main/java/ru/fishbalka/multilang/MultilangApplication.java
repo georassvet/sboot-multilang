f01b7f9d7a67003dbc2d81b1ef562d9610fed213
Sergey Mokhov <s.v.mokhov@gmail.com>
2019-02-28 12:41:55 +0300

    init

---
+++ src/main/java/ru/fishbalka/multilang/MultilangApplication.java
@@ -0,0 +1,13 @@
+package ru.fishbalka.multilang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultilangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultilangApplication.class, args);
+    }
+
+}
